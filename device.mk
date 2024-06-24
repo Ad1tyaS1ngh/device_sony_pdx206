4467bc6a7e369dc7d82ba080e3989debe7939de3
Luofan Chen <clfbbn@gmail.com>
2024-06-24 08:36:21 +0000

    pdx206: Declare wlan firmware files as symlinks during extraction
    Depends-On: Ie9b196e164d89eed16d20b8a9b68f2a6329d5605
    Change-Id: I67dcbb70047721792823f73644d3c6541b38d923

--- device.mk
+++ device.mk
@@ -552,10 +552,6 @@ PRODUCT_COPY_FILES += \
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
-    firmware_regdb.bin_symlink \
-    firmware_bdwlan.e16_symlink \
-    firmware_bdwlan.e17_symlink \
-    firmware_bdwlan.elf_symlink \
     firmware_wlanmdsp.otaupdate_symlink \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
