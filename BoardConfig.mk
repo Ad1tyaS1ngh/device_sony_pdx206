95ecffa9995e4d648390e01b60ed46de7ff003a9
ad1tyas1ngh <adityasingh1768@gmail.com>
2024-09-06 08:47:16 +0000

    pdx206: sepolicy: Drop duplicate labels
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,7 +202,6 @@ VENDOR_SECURITY_PATCH := 2022-10-01
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
