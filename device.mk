fb4258c0e0187c440f0d79688fcaa32d4bf9cc17
Michael Bestas <mkbestas@lineageos.org>
2025-08-07 18:22:09 +0000

    pdx206: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- device.mk
+++ device.mk
@@ -164,7 +164,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.kona \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
